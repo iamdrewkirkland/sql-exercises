3a0cbcaeb93ce3904f0eac033c662d0dcc5db528
Drew Kirkland <drew.kirkland@gmail.com>
2020-06-02 16:00:59 -0400

    Most queries complete with small issue on INSERT.

--- MusicHistory/MusicHistoryQueries.sql
+++ MusicHistory/MusicHistoryQueries.sql
@@ -1,5 +1,5 @@
 ﻿--Query all of the entries in the Genre table
-SELECT * FROM Genre
+--SELECT * FROM Genre
 
 --Using the INSERT statement, add one of your favorite artists to the Artist table.
 --INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('The Floozies', '2013')
@@ -17,17 +17,40 @@ SELECT * FROM Genre
 
 
 --Write a SELECT statement to display how many songs exist for each album. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
-SELECT album.Title, COUNT(song.Id) AS 'Song Count'
-FROM Album album
-JOIN Song song on song.AlbumId = album.Id
-GROUP BY (album.Title)
+--SELECT album.Title, COUNT(song.Id) AS 'Song Count'
+--FROM Album album
+--JOIN Song song on song.AlbumId = album.Id
+--GROUP BY (album.Title)
 
 --Write a SELECT statement to display how many songs exist for each artist. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+--SELECT artist.ArtistName, COUNT(song.Id) AS 'Song Count'
+--FROM Artist artist
+--JOIN Song song on song.ArtistId = artist.Id
+--GROUP BY (artist.ArtistName)
 
 --Write a SELECT statement to display how many songs exist for each genre. You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+--SELECT genre.[Label], COUNT(song.Id) AS 'Song Count'
+--FROM Genre genre
+--JOIN Song song on song.GenreId = genre.Id
+--GROUP BY (genre.[Label])
 
 --Using MAX() function, write a select statement to find the album with the longest duration. The result should display the album title and the duration.
+--SELECT *
+--FROM Album
+--WHERE AlbumLength = (SELECT MAX(AlbumLength) FROM Album)
+
+
 
 --Using MAX() function, write a select statement to find the song with the longest duration. The result should display the song title and the duration.
+--SELECT 
+--	song.Title AS 'Song',
+--	song.SongLength,
+--	song.ReleaseDate,
+--	album.Title AS 'Album Title',
+--	album.[Label] AS 'Record Label'
+--FROM Song song
+--JOIN Album album ON song.AlbumId = album.Id
+--WHERE song.SongLength = (SELECT MAX(SongLength) FROM Song)
+
 
 --Modify the previous query to also display the title of the album.
